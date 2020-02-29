afbe215df1c70254e24fe1288577763df98a54df
DanSample <40527003+DanSample@users.noreply.github.com>
2020-02-29 14:28:43 -0800

    set up knexfile for postgres db

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,40 @@
+require("dotenv").config();
 module.exports = {
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./database/choretracker.db3"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    },
-    migrations: {
-      directory: "./database/migrations"
-    },
-    seeds: {
-      directory: "./database/seeds"
-    }
-  }
+	development: {
+		client: "pg",
+		connection: {
+			connectionString: process.env.DB_CONNECTION_URI,
+		},
+		migrations: {
+			directory: "./database/migrations",
+		},
+		seeds: {
+			directory: "./database/seeds",
+		},
+	},
+	production: {
+		client: "pg",
+		connection: {
+			connectionString: process.env.DATABASE_URL,
+			ssl: true,
+		},
+		migrations: {
+			directory: "./database/migrations",
+		},
+		seeds: {
+			directory: "./database/seeds",
+		},
+	},
+	testing: {
+		client: "sqlite3",
+		connection: {
+			filename: "./database/test.db3",
+		},
+		migrations: {
+			directory: "./database/migrations",
+		},
+		seeds: {
+			directory: "./database/seeds",
+		},
+	},
 };
