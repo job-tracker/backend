55e6a40c5e1eba396c03109c3c2a605287fd1bc8
DanSample <40527003+DanSample@users.noreply.github.com>
2020-02-29 15:02:00 -0800

    built out jobsite and building tables

--- database/migrations/20200229144535_create_tables.js
+++ database/migrations/20200229144535_create_tables.js
@@ -1,8 +1,40 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.creatTable('jobsite', tbl => {
+    tbl.increments();
+    tbl.string('name');
+    tbl.string('address');
+    tbl.jsonb('contact');
+    tbl.string('notes');
+    tbl.boolean('complete');
+  })
+
+  .creatTable('building', tbl => {
+    tbl.increments();
+    tbl.integer('jobsite_id').references('jobsite.id');
+    tbl.string('name');
+    tbl.integer('floor');
+    tbl.integer('unit_count');
+    tbl.integer('outlet_count');
+    tbl.boolean('complete');
+  })
+
+  .createTable()
 };
 
 exports.down = function(knex) {
   
 };
+
+return knex.schema
+		.createTable("couples", table => {
+			table.increments();
+			table.string("spouse_one_name").notNullable();
+			table.string("spouse_two_name").notNullable();
+			table
+				.string("email")
+				.notNullable()
+				.unique();
+			table.string("password").notNullable();
+			table.string("jwt", 512);
+		})
